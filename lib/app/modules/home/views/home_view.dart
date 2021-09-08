83283da82aeb4988a886498162b1648745166ef8
Irfan Maulana <irfan.resimen2018@gmail.com>
2021-09-08 17:11:10 +0700

    dropdown test

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -8,12 +9,24 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('HomeView'),
-          centerTitle: true,
-        ),
-        body: ListView(
-          children: [],
-        ));
+      appBar: AppBar(
+        title: Text('HomeView'),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          DropdownSearch<String>(
+              mode: Mode.BOTTOM_SHEET,
+              showSelectedItem: true,
+              items: ["Brazil", "Italia (Disabled)", "Tunisia", 'Canada'],
+              label: "Menu mode",
+              hint: "country in menu mode",
+              popupItemDisabled: (String s) => s.startsWith('I'),
+              onChanged: print,
+              selectedItem: "Brazil"),
+        ],
+      ),
+    );
   }
 }
